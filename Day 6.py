27295bf2ebbe5b77777a4105a0b7aac6d46ecad0
Ibrahim Yusuf <yusufkorede741@gmail.com>
2025-01-12 11:42:40 -0500

    Add files via upload

---
+++ Day 6.py
@@ -0,0 +1,14 @@
+numbers = []
+total = 0
+
+while True:
+    number = int(input("Please input a number (Enter 0 to exit): "))
+    if number == 0:
+        break
+    else:
+        numbers.append(number)
+        print(numbers)
+total = sum(numbers)
+average = sum(numbers) / len(numbers)
+print(f"The total is: {total}")
+print(f"The average is: {average}")
