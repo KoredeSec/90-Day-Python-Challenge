111af304d50481ea71f096a0602e0a443d7b2210
Ibrahim Yusuf <yusufkorede741@gmail.com>
2025-01-13 17:03:59 -0500

    Add files via upload

---
+++ Day 7.py
@@ -0,0 +1,8 @@
+#program that stores user information (name, age) in a dictionary and allows the user to retrieve it by providing the name
+
+User_information = {"Tory": "Tory Sec, 18",
+                    "Jane": "Jane Doe, 20",
+                    "John": "John Doe, 21",
+                    "Anon": "Anony mous, 25"}
+
+print(User_information.get("Tory"))
