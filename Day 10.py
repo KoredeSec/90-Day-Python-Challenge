58b8667edd9eebf0134798cba24fa8984ebcf2fe
Ibrahim Yusuf <yusufkorede741@gmail.com>
2025-01-16 15:33:51 -0500

    Add files via upload

---
+++ Day 10.py
@@ -0,0 +1,9 @@
+#Python script that calculates the square root of a number using the math library.
+
+import math
+
+name = (input("Hello!, What is your name?: "))
+print(f"Hello! {name}")
+number = int(input("Please input a number?: "))
+result = math.sqrt(number)
+print(result)
